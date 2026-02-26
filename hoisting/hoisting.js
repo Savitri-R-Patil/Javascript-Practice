89fde46d85d20b51b3355cf8626b69aac48c0e42
Savitri R Patil <savitri033@gmail.com>
2026-02-26 20:13:52 +0530

    feat: added function and hosting folder

---
+++ hoisting/hoisting.js
@@ -0,0 +1,10 @@
+ /*hoising*/
+ 
+ greet();
+ console.log(x);
+ 
+ var x=9;
+ function greet(){
+    console.log('hi! , Welcome');
+ }
+ 
---
+++ index.js
@@ -0,0 +1,11 @@
+var x=7
+
+function greet(){
+    console.log('Namste saa');
+
+}
+greet();
+console.log(x);
+
+console.log(greet)
+
